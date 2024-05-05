1525a5e7e7800f4d6b1719dd068599585afeac06
shivanaguttiadi <143313733+shivanaguttiadi@users.noreply.github.com>
2024-05-05 13:47:00 +0530

    Delete dockerinstall.sh

--- dockerinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Update system packages
-sudo apt update
-sudo apt upgrade -y
-# Update Docker containers (if applicable)
-docker-compose -f /path/to/docker-compose.yaml pull
-docker-compose -f /path/to/docker-compose.yaml up -d
